c503a42d948d1d888599c30c979a193c853daf54
ianouu <clementian067@gmail.com>
2019-02-26 20:59:33 +0100

    first commit

---
+++ action-Turn-off-light.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import ConfigParser
+from hermes_python.hermes import Hermes
+from hermes_python.ontology import *
+import io
+
+import simplejson
+import requests
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIG_INI = "config.ini"
+
+class SnipsConfigParser(ConfigParser.SafeConfigParser):
+    def to_dict(self):
+        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+
+
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, ConfigParser.Error) as e:
+        return dict()
+
+def subscribe_intent_callback(hermes, intentMessage):
+    conf = read_configuration_file(CONFIG_INI)
+    action_wrapper(hermes, intentMessage, conf)
+
+
+def action_wrapper(hermes, intentMessage, conf):
+    """
+    TURN OFF
+    """
+    addr_ = conf['global']['ip']
+    port_ =conf['global']['port']
+
+    
+        
+    
+    try:           
+        request = "turn/off/all"
+		url = "http://" + addr_ + ":" + port_ + "/" + request		
+		response = requests.get(url)
+		
+        hermes.publish_end_session(intentMessage.session_id, "")
+    except requests.exceptions.RequestException:
+        hermes.publish_end_session(intentMessage.session_id, "Erreur de connection.")
+    except Exception:
+        hermes.publish_end_session(intentMessage.session_id, "Erreur de l'application.")
+
+
+
+if __name__ == "__main__":
+    with Hermes("localhost:1883") as h:
+        h.subscribe_intent("Ianou:Turn-off-light", subscribe_intent_callback) \
+         .start()
